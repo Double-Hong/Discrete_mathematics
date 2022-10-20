5379567f36aa773804c3be4b749058e1b5fda388
DoubleHong <greathongtian@gmail.com>
2022-10-20 14:18:13 +0800

    Hong

---
+++ src/mainfun.java
@@ -0,0 +1,9 @@
+public class mainfun {
+    public static void main(String[] args) {
+        String A = "abc";
+        System.out.println(A);
+        for (int i=0;i<3;i++){
+            System.out.println(A.charAt(i));
+        }
+    }
+}
